a37bc67cd9cd5f2c0de2244388413b1f3ac559fa
Nayan Jyoti Das <dastms@gmail.com>
2024-11-02 21:53:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,7 +72,8 @@ fi
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .p10k.zsh ~/.p10k.zsh
 
-source ~/.zshrc
+# Reload zshrc
+zsh
 
 # Install Alacritty theme
 mkdir -p ~/.config/alacritty/themes
