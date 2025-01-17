9706f0b9c8ff52ce0f55739b4ce3627a583e3312
Nayan Das <dastms@gmail.com>
2025-01-17 12:22:19 +0000

    Add ripgrep to debian

--- setup-debian.sh
+++ setup-debian.sh
@@ -11,7 +11,7 @@ function setup_debian() {
     
     apt-get update && \
       apt-get install -y git tmux tmuxinator zsh zsh-syntax-highlighting && \
-      apt-get install -y direnv neovim
+      apt-get install -y direnv neovim ripgrep
     
     
     git config --global user.email "dastms@gmail.com"
