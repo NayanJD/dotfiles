1b99b626c5bebc964e3a4aacfa3b54f7c12dfe1c
Nayan Das <ndas@digitalocean.com>
2025-03-16 21:25:25 +0530

    Add lima vm config. Change setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -18,9 +18,9 @@ function setup_debian() {
     apt-get update && \
       apt-get install -y git tmux tmuxinator zsh zsh-syntax-highlighting && \
       apt-get install -y direnv ripgrep nodejs npm unzip jq && \
-      # apt-get install -y libtool autoconf automake cmake libncurses5-dev g++ gettext && \
       apt-get install -y neofetch kitty && \ 
       apt-get install -y python3.10-venv
+      # apt-get install -y libtool autoconf automake cmake libncurses5-dev g++ gettext && \
       
     
     export HOME=/root
